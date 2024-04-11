e418a2b518319e3c3429f6a76317ebff42098a6e
Dawid Cybulski <100097374+davidcybulsky@users.noreply.github.com>
2024-04-11 16:43:02 +0200

    Game works

--- MainMappingProfile.cs
+++ MainMappingProfile.cs
@@ -12,6 +12,12 @@ namespace UserDataService
             CreateMap<Statistics, StatisticsDto>();
             CreateMap<Game, GameDto>();
             CreateMap<User, FriendDto>();
+            CreateMap<CreateGameDto, Game>()
+                .ForMember(dest => dest.BlackId, opt => opt.MapFrom(src => src.blackUserId))
+                .ForMember(dest => dest.WhiteId, opt => opt.MapFrom(src => src.whiteUserId))
+                .ForMember(dest => dest.Mode, opt => opt.MapFrom(src => src.gameMode))
+                .ForMember(dest => dest.Win, opt => opt.MapFrom(src => src.gameStatus))
+                .ForMember(dest => dest.Moves, opt => opt.MapFrom(src => src.moveList));
         }
     }
 }
---
+++ Models/CreateGameDto.cs
@@ -0,0 +1,14 @@
+﻿namespace UserDataService.Models
+{
+    public class CreateGameDto
+    {
+        public string gameCode { get; set; }
+        public string whiteUserId { get; set; }
+        public string blackUserId { get; set; }
+        public string gameMode { get; set; }
+        public string gameTime { get; set; }
+        public string gameStatus { get; set; }
+        public List<string> fenList { get; set; }
+        public List<string> moveList { get; set; }
+    }
+}

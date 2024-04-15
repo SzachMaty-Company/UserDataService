6d62ad9907613c4a20cd20ca168314a73eefac64
Dawid Cybulski <100097374+davidcybulsky@users.noreply.github.com>
2024-04-15 17:54:01 +0200

    Getting rid of datetime conversation

--- MainMappingProfile.cs
+++ MainMappingProfile.cs
@@ -15,7 +15,7 @@ namespace UserDataService
             CreateMap<CreateGameDto, Game>()
                 .ForMember(dest => dest.BlackId, opt => opt.MapFrom(src => src.blackUserId))
                 .ForMember(dest => dest.WhiteId, opt => opt.MapFrom(src => src.whiteUserId))
-                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(src.gameStartTime), TimeZoneInfo.Local)))
+                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.gameStartTime))
                 .ForMember(dest => dest.Mode, opt => opt.MapFrom(src => src.gameMode))
                 .ForMember(dest => dest.Win, opt => opt.MapFrom(src => src.gameStatus))
                 .ForMember(dest => dest.Moves, opt => opt.MapFrom(src => src.moveList));
